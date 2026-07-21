ab879423643fcc67055e5ddf97298aa791ccaafc
Suraj Patil <surajpatil3037@gmail.com>
2026-07-21 22:43:09 +0530

    Fix: Remove non-existent address field from staff register template - fixes 500 error

--- build_files.sh
+++ build_files.sh
@@ -8,9 +8,6 @@ echo "================================"
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Running database migrations..."
-python manage.py migrate --noinput
-
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear --verbosity 2
 
